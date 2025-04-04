5a9292394f41207f3993669111f3dee32dd18136
2256costa <rodrigo5592@yahoo.com.br>
2025-04-04 18:42:48 -0300

    A

--- SGP/core/urls.py
+++ SGP/core/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import
-from django.urls import path
+from django.contrib import admin
 from django.views.generic import RedirectView
+from django.urls import path
+
 
 urlpatterns = [
 
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
+
     path('', admin.site.urls),
 
-    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
+    
 ]
+
+
 
 
